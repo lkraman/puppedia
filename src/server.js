bd4d569830782477b046100e2a6ea251cba7d2aa
lkraman <laurakraman01@gmail.com>
2019-06-04 15:46:02 -0400

    commit changes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const http = require('http');
 
-const port = normalizePort(process.env.PORT || '5000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
