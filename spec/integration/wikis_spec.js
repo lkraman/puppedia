10fbde8145f2bcbbb8cfdf937cbfae37793faec5
lkraman <laurakraman01@gmail.com>
2019-06-04 14:02:51 -0400

    commit

--- spec/integration/wikis_spec.js
+++ spec/integration/wikis_spec.js
@@ -5,134 +5,68 @@ const sequelize = require("../../src/db/models/index").sequelize;
 const Wiki = require("../../src/db/models").Wiki;
 const User = require("../../src/db/models").User;
 
-describe("routes : wikis", () => {
+describe("routes : posts", () => {
+
   beforeEach((done) => {
-    this.user;
     this.wiki;
+    this.user;
 
     sequelize.sync({force: true}).then((res) => {
-
       User.create({
-          username: "lauraloo",
-          email: "user@example.com",
-          password: "password"
-      })
-      .then((user) => {
-        this.user = user;
-
-        Wiki.create({
-            title: "Basenji",
-            body: "A Basenji is known as a barkless dog",
-            userId: this.user.id
-        })
-        .then((wiki) => {
-          this.wiki = wiki;
-          done();
-        })
-        .catch((err) => {
-          console.log(err);
-          done();
-        });
-      });
-    });
+        username: "lauraloo",
+        email: "user@example.com",
+        password: "password"
+     })
+     .then((user) => {
+       this.user = user;
+       Wiki.create({
+        title: "Baseji",
+        body: "They are known as the barkless dog",
+        UserId: user.id
+       })
+       .then((wiki) => {
+         this.wiki = wiki;
+         done();
+       })
+     })
+   });
   });
 
-  describe("GET /wikis", () => {
-    it("should render a view with the selected wiki", (done) => {
-      request.get(base, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("Basenji");
-        done();
+  describe("user trying to create a wiki", () => {
+    beforeEach((done) => {
+      request.get({
+        url: "http://localhost:3000/mock/auth",
+        form: {
+          role: this.user.role,
+          userId: this.user.id,
+          email: this.user.email
+        }
       });
+      done();
     });
-  });
 
-  describe("GET /wikis/new", () => {
-    it("should render a new wiki form", (done) => {
-      request.get(`${base}new`, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("New Wiki");
-        done();
-      });
-    });
-  });
-
-  describe("POST /wikis/create", () => {
-    const options = {
-      url: `${base}create`,
-      form: {
-        title: "Chinchillas",
-        body: "they like dust baths"
-      }
-    };
-
-
-    it("should create a new wiki with a title and body", (done) => {
-      request.post(options, (err, res, body) => {
-        Wiki.findOne({where: {title: "Chinchillas"}})
-        .then((wiki) => {
-          expect(wiki.title).toBe("Chinchillas");
-          expect(wiki.body).toBe("they like dust baths");
-          done();
-        })
-        .catch((err) => {
-          console.log(err);
+    describe("GET /wikis/:id", () => {
+      it("should render a view with the selected wiki", (done) => {
+        request.get(`${base}/${this.wiki.id}`, (err, res, body) => {
+          expect(err).toBeNull();
+          expect(body).toContain("Basenji");
           done();
         });
       });
     });
   });
- 
 
   describe("POST /wikis/:id/destroy", () => {
     it("should delete the wiki with the associated ID", (done) => {
-      Wiki.findAll()
-      .then((wikis) => {
-        const wikiCountBeforeDelete = wikis.length;
-        expect(wikiCountBeforeDelete).toBe(1);
+      expect(this.wiki.id).toBe(1);
 
-        request.post(`${base}${this.wiki.id}/destroy`, (err, res, body) => {
-          Wiki.findAll()
-          .then((wikis) => {
-            expect(err).toBeNull();
-            expect(wikis.length).toBe(wikiCountBeforeDelete - 1);
-            done();
-          });
-        });
-      });
-    });
-  });
-
-  describe("GET /wikis/:id/edit", () => {
-    it("should render a view with an edit wiki form", (done) => {
-      request.get(`${base}${this.wiki.id}/edit`, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("Edit Wiki");
-        expect(body).toContain("Basenji");
-        done();
-      });
-    });
-  });
-
-  describe("POST /wikis/:id/update", () => {
-    it("should update the wiki with the given values", (done) => {
-      const options = {
-        url: `${base}${this.wiki.id}/update`,
-        form: {
-          title: "Cats",
-          body: "Cats cannot detect sweetness."
-        }
-      };
-
-      request.post(options, (err, res, body) => {
-        expect(err).toBeNull();
-        Wiki.findOne({
-          where: { id: this.wiki.id }
-        })
+      request.post(`${base}/${this.wiki.id}/destroy`, (err, res, body) => {
+        Wiki.findByPk(1)
         .then((wiki) => {
-          expect(wiki.title).toBe("Cats");
+          expect(err).toBeNull();
+          expect(wiki).toBeNull();
           done();
-        });
+        })
       });
     });
   });
--- spec/unit/wiki_spec.js
+++ spec/unit/wiki_spec.js
@@ -27,78 +27,45 @@ describe("Wiki", () => {
             .then((wiki) => {
               this.wiki = wiki;
               done();
-            })
-            .catch((err) => {
-              console.log(err);
-              done();
             });
           });
         });
       });
     
       describe("#create()", () => {
-        it("should create a wiki object with title, body and assigned user", (done) => {
+        it("should create a wiki object with a title, body", (done) => {
           Wiki.create({
-            _title: "Cats",
-            get title() {
-              return this._title;
-            },
-            set title(value) {
-              this._title = value;
-            },
-            body: "Cats can rotate their ears 180 degrees.",
-            userId: this.user.id
+            title: "Cats",
+            body: "Cats can turn their ears 180 degrees",
+            UserId: this.user.id
           })
           .then((wiki) => {
             expect(wiki.title).toBe("Cats");
-            expect(wiki.body).toBe("Cats can rotate their ears 180 degrees.");
+            expect(wiki.body).toBe("Cats can turn their ears 180 degrees");
+            expect(wiki.UserId).toBe(this.user.id);
             done();
+    
           })
           .catch((err) => {
             console.log(err);
             done();
           });
         });
+      });
     
-        it("should not create a wiki object with missing title or body", (done) => {
-          Wiki.create({
-            title: "Cats"
-          })
+      describe("#destroy()", () => {
+        it("should delete the specified wiki", (done) => {
+          Wiki.destroy({where: {id: this.wiki.id}})
           .then((wiki) => {
+            expect(wiki.title).toBe(undefined);
             done();
+    
           })
           .catch((err) => {
-            expect(err.message).toContain("Wiki.body cannot be null");
+            console.log(err);
             done();
           });
         });
       });
     
-      describe("#setUser()", () => {
-        it("should associate a wiki and a user together", (done) => {
-          User.create({
-            username: "fakeusername",
-            email: "fakeuser@example.com",
-            password: "123456"
-          })
-          .then((newUser) => {
-            expect(this.wiki.userId).toBe(this.user.id);
-            this.wiki.setUser(newUser)
-            .then((wiki) => {
-              expect(this.wiki.userId).toBe(newUser.id);
-              done();
-            });
-          });
-        });
-      });
-    
-      describe("#getUser()", () => {
-        it("should return the associated user", (done) => {
-          this.wiki.getUser()
-          .then((associatedUser) => {
-            expect(associatedUser.email).toBe("user@example.com");
-            done();
-          });
-        });
-      });
     });
