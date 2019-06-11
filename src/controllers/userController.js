a77108f62ded093315145a37c1a452430ae589a4
lkraman <laurakraman01@gmail.com>
2019-06-11 11:25:25 -0400

    commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,34 +18,30 @@ module.exports = {
     };
 
     userQueries.createUser(newUser, (err, user) => {
-      if (err) {
+      if(err) {
+        console.log(err);
         req.flash("error", err);
         res.redirect("/users/sign_up");
-      } else {
-        //send email
-        sgMail.setApiKey('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');
+      }
+      else {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
-          to: req.body.email,
-          from: 'gray2884@gmail.com',
-          subject: "You've created an account on Blocipiedia",
-          text: 'Thanks for joining!',
-          html: '<strong>Hope to see you around!</strong>',
+          to: newUser.email,
+          from: 'laurakraman01@gmail.com',
+          subject: 'May31 test for Blocipedia',
+          text: 'test',
+          html: '<strong>test!</strong>',
         };
-
         sgMail.send(msg);
 
-        //sign in
         passport.authenticate("local")(req, res, () => {
           req.flash("notice", "You've successfully signed in!");
           res.redirect("/");
         });
       }
     });
-  },
-
-  signInForm(req, res, next) {
-    res.render("users/sign_in");
-  },
+  }
+}
 
   signIn(req, res, next) {
     passport.authenticate("local")(req, res, function () {
