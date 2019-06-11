7498630c7666c13858736a9dfcce0d8026c77054
lkraman <laurakraman01@gmail.com>
2019-06-11 11:44:22 -0400

    commit

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const validation = require("./validation");
 const userController = require("../controllers/userController")
 
 router.get("/users/sign_up", userController.signUp);
-router.get("/users/sign_in", userController.signInForm);
+router.get("/users/sign_in", userController.signIn);
 router.get("/users/sign_out", userController.signOut);
 
 router.post("/users", validation.validateUsers, userController.create);
