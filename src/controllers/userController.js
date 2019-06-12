7cf1e2e5534599fba46e6b2e66b83a738addc86c
lkraman <laurakraman01@gmail.com>
2019-06-12 10:12:58 -0400

    commit checkpoint user roles

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,13 @@
 const userQueries = require("../db/queries.users.js");
 const passport = require("passport");
-const sgMail = require('@sendgrid/mail');
+const sgMail = require("@sendgrid/mail");
 
 module.exports = {
-
   signUp(req, res, next) {
     res.render("users/sign_up");
   },
 
   create(req, res, next) {
-
     let newUser = {
       username: req.body.username,
       email: req.body.email,
@@ -18,23 +16,22 @@ module.exports = {
     };
 
     userQueries.createUser(newUser, (err, user) => {
-      if (err) {
+      if(err) {
+        console.log(err);
         req.flash("error", err);
         res.redirect("/users/sign_up");
-      } else {
-        //send email
-        sgMail.setApiKey('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');
+      }
+      else {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
-          to: req.body.email,
-          from: 'gray2884@gmail.com',
-          subject: "You've created an account on Blocipiedia",
-          text: 'Thanks for joining!',
-          html: '<strong>Hope to see you around!</strong>',
+          to: newUser.email,
+          from: 'laurakraman01@gmail.com',
+          subject: 'May31 test for Blocipedia',
+          text: 'test',
+          html: '<strong>test!</strong>',
         };
-
         sgMail.send(msg);
 
-        //sign in
         passport.authenticate("local")(req, res, () => {
           req.flash("notice", "You've successfully signed in!");
           res.redirect("/");
@@ -42,28 +39,26 @@ module.exports = {
       }
     });
   },
-
-  signInForm(req, res, next) {
-    res.render("users/sign_in");
-  },
-
   signIn(req, res, next) {
     passport.authenticate("local")(req, res, function () {
-      if (!req.user) {
+      if(!req.user) {
         req.flash("notice", "Sign in failed. Please try again.");
         res.redirect("/users/sign_in");
-      } else {
-        req.flash("notice", "You've successfully signed in!");
+      }
+      else {
+        req.flash("notice", "You've successfully signed in.");
         res.redirect("/");
       }
-    })
+    });
   },
+
+  signInForm(req, res, next){
+    res.render("users/sign_in");
+  }, 
 
   signOut(req, res, next) {
     req.logout();
-    req.flash("notice", "You've successfully signed out!");
+    req.flash("notice", "You've successfully signed out.");
     res.redirect("/");
-  },
-
-
+  }
 }
