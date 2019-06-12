8f9af74f86ed45a4bcb3fb427315496a253785b2
lkraman <laurakraman01@gmail.com>
2019-06-12 10:36:02 -0400

    fixed bad user request

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,11 +4,10 @@ const validation = require("./validation");
 const userController = require("../controllers/userController")
 
 router.get("/users/sign_up", userController.signUp);
-router.get("/users/sign_in", userController.signIn);
+router.get("/users/sign_in", userController.signInForm);
 router.get("/users/sign_out", userController.signOut);
 
 router.post("/users", validation.validateUsers, userController.create);
 router.post("/users/sign_in", validation.validateUsersSignIn, userController.signIn);
 
 module.exports = router;
-
