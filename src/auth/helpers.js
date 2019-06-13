4e47b70f640a3e61362fda660a7e13ef7b751160
lkraman <laurakraman01@gmail.com>
2019-06-13 09:51:22 -0400

    commit

--- src/auth/helpers.js
+++ src/auth/helpers.js
@@ -3,10 +3,12 @@ const bcrypt = require("bcryptjs");
 module.exports = {
   ensureAuthenticated(req, res, next) {
     if(!req.user) {
+      console.log("HELPERSJS ERRORS PAGE");
       req.flash("notice", "You must be signed in to do that.")
       return res.redirect("/users/sign_in");
     }
     else {
+      console.log("SUCCESS ON HELPERSJS");
       next();
     }
   },
