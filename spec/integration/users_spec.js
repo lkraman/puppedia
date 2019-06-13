3e9d1aa210db0a46ca1cc7ef8fca88b4cd99994c
lkraman <laurakraman01@gmail.com>
2019-06-13 12:14:33 -0400

    commit

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -20,71 +20,70 @@ describe("routes : users", () => {
     it("should render a view with a sign up form", (done) => {
       request.get(`${base}sign_up`, (err, res, body) => {
         expect(err).toBeNull();
-        expect(body).toContain("Welcome to Blocipedia")
+        expect(body).toContain("Sign Up")
         done();
       });
     });
   });
 
   describe("POST /users", () => {
-    it("should create a user with valid username, email, and password", (done) => {
-      const options = {
-        url: base,
-        form: {
-          username: "lauraloo",
-          email: "user@example.com",
-          password: "password"
-        }
-      }
-      request.post(options, (err, res, body) => {
-        User.findOne({where: {email: "user@example.com"}})
-        .then((user) => {
-          expect(user).not.toBeNull();
-          expect(user.username).toBe("lauraloo")
-          expect(user.email).toBe("user@example.com");
-          expect(user.id).toBe(1);
-          done();
-        })
-        .catch((err) => {
-          console.log(err);
-          done();
+    it("should create a new user with valid values and redirect", done => {
+        const options = {
+            url: base,
+            form: {
+                username: "username",
+                email: "user@example.com",
+                password: "123456789"
+            }
+        };
+
+        request.post(options, (err, res, body) => {
+            User.findOne({ where: { email: "user@example.com" } })
+                .then(user => {
+                    expect(user).not.toBeNull();
+                    expect(user.email).toBe("user@example.com");
+                    expect(user.id).toBe(1);
+                    done();
+                })
+                .catch(err => {
+                    console.log(err);
+                    done();
+                });
         });
-      });
     });
 
-    it("should not create a user with invalid values and redirect", (done) => {
+    it("should not create a new user with invalid attributes and redirect", done => {
       request.post(
-        {
-          url: base,
-          form: {
-            username: "blah",
-            email: "invalid",
-            password: "password"
+          {
+              url: base,
+              form: {
+                  email: "no",
+                  password: "123456789"
+              }
+          },
+          (err, body, res) => {
+              User.findOne({ where: { email: "no" } })
+                  .then(user => {
+                      expect(user).toBeNull();
+                      done();
+                  })
+                  .catch(err => {
+                      console.log(err);
+                      done();
+                  });
           }
-        },
-        (err, res, body) => {
-          User.findOne({where: {email: "invalid"}})
-          .then((user) => {
-            expect(user).toBeNull();
-            done();
-          })
-          .catch((err) => {
-            console.log(err);
-            done();
-          });
-        }
       );
-    });
-  });
-
-  describe("GET /users/sign_in", () => {
-    it("should render a view with a Sign In form", (done) => {
-      request.get(`${base}sign_in`, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("Sign In")
-        done();
-      });
-    });
   });
 });
 
+
+describe("GET /users/sign_in", () => {
+  it("should render a view with a sign in form", done => {
+      request.get(`${base}sign_in`, (err, res, body) => {
+          expect(err).toBeNull();
+          expect(body).toContain("Sign in");
+          done();
+      });
+  });
+});
+});
