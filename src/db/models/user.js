ce97288420493af146c735cf896a6a9149006290
lkraman <laurakraman01@gmail.com>
2019-06-13 12:53:26 -0400

    commit

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -8,9 +8,6 @@ module.exports = (sequelize, DataTypes) => {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        isEmail: { msg: "must be a valid email"}
-      }
     },
     password: {
       type: DataTypes.STRING,
